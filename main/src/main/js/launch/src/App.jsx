f549dbea0dc02283dabb4925e559667db0227d2f
Eldon Ahrold <ahrolde@objectcomputing.com>
2021-02-04 15:45:37 -0600

    Branding style updates (Feb 2021) (#24)
    * [MNSUI-23] Update Theme Colors Solid Black/White
    
    * [MNSUI-23] Update fonts, font size and font weights
    
    * [MNSUI-23] Update OCI Logo, keep accent colors across theme changes
    
    * [MNSUI-23] Update Micronaut Header Logo

--- main/src/main/js/launch/src/App.jsx
+++ main/src/main/js/launch/src/App.jsx
@@ -29,6 +29,7 @@ import { downloadBlob, makeNodeTree } from './utility'
 import './style.css'
 import './styles/button-row.css'
 import './styles/modal-overrides.css'
+import './styles/font-overrides.css'
 import './styles/utility.css'
 
 import { parseQuery } from './helpers/url'
