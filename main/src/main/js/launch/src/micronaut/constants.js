ebd5595de2a81a7345e3e7efcbccdc74c8545a8b
jameskleeh <james.kleeh@gmail.com>
2021-08-18 13:02:36 -0400

    Support a previous release

--- main/src/main/js/launch/src/micronaut/constants.js
+++ main/src/main/js/launch/src/micronaut/constants.js
@@ -7,12 +7,17 @@ export const SNAPSHOT_API_URL =
 export const NEXT_API_URL =
   process.env.REACT_APP_NEXT_API_URL || 'https://next.micronaut.io'
 
+export const PREV_API_URL =
+  process.env.REACT_APP_PREV_API_URL || 'https://prev.micronaut.io'
+
 const RELEASE = 'RELEASE'
 const SNAPSHOT = 'SNAPSHOT'
 const NEXT = 'NEXT'
+const PREV = 'PREV'
 
 export const DEFAULT_APIS = {
   [RELEASE]: { baseUrl: API_URL, key: RELEASE, order: 0 },
   [SNAPSHOT]: { baseUrl: SNAPSHOT_API_URL, key: SNAPSHOT, order: 1 },
-  [NEXT]: { baseUrl: NEXT_API_URL, key: NEXT, order: 2 },
+  [PREV]: { baseUrl: PREV_API_URL, key: PREV, order: 2 },
+  [NEXT]: { baseUrl: NEXT_API_URL, key: NEXT, order: 3 },
 }
